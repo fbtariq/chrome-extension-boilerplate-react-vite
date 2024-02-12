da0cd0dcb23fde0a771de4ffb83eee947ef1ebb0
Fahad <fahadtariq@gmail.com>
2024-02-12 17:41:32 -0500

    Update Popup.tsx

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,5 +1,18 @@
-import { Button } from "@chakra-ui/react";
+import { createClient, User } from "@supabase/supabase-js";
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 
-export default function Popup() {
-  return <Button colorScheme="teal">Button</Button>;
+// get auth url
+export async function signInWithGoogle() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "google"
+  });
+
+  // tell background service worker to create a new tab with that url
+  await chrome.runtime.sendMessage({
+    action: "signInWithGoogle",
+    payload: { url: data.url } // url is something like: https://[project_id].supabase.co/auth/v1/authorize?provider=google
+  });
 }
